fb773fd0b0742bd346f1bce46f21f004e47d8b42
Mikhail Kruk <mi.kruk95@gmail.com>
2026-03-17 14:18:32 +0100

    feat: initial commit

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -2,13 +2,16 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReduxProvider from './redux-provider';
 import TanstakqueryProvider from './tanstackquery-provider';
+import ThemeProvider from './theme-provider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ReduxProvider>
 			<TanstakqueryProvider>
-				{children}
-				<ReactQueryDevtools initialIsOpen={false} />
+				<ThemeProvider>
+					{children}
+					<ReactQueryDevtools initialIsOpen={false} />
+				</ThemeProvider>
 			</TanstakqueryProvider>
 		</ReduxProvider>
 	);
